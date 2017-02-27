31f8e2ea9ce0e83ad0f65d64a23184037f4901a9
Pilaksan <Pilaksan@PILAKSAN_PC>
2017-02-27 22:16:49 +0100

    Projektdateien hinzufügen.

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(507, 163);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(77, 223);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(366, 223);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(77, 298);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(366, 298);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(208, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 364);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        int index = 0;
+        
+        List<QuestionAndAnswers> Questions = new List<QuestionAndAnswers>();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            
+            
+            Questions.Add(new QuestionAndAnswers() { Question = "Frage 1", Answers = "Antwort 1", Answers0 = "Antwort 2", Answers1 = "Antwort 3", Answers2 = "Antwort 4" });
+            Questions.Add(new QuestionAndAnswers() { Question = "Frage 2", Answers = "Antwort 1", Answers0 = "Antwort 2", Answers1 = "Antwort 3", Answers2 = "Antwort 4" });
+            Questions.Add(new QuestionAndAnswers() { Question = "Frage 3", Answers = "Antwort 1", Answers0 = "Antwort 2", Answers1 = "Antwort 3", Answers2 = "Antwort 4" });
+            Questions.Add(new QuestionAndAnswers() { Question = "Frage 4", Answers = "Antwort 1", Answers0 = "Antwort 2", Answers1 = "Antwort 3", Answers2 = "Antwort 4" });
+            Questions.Add(new QuestionAndAnswers() { Question = "Frage 5", Answers = "Antwort 1", Answers0 = "Antwort 2", Answers1 = "Antwort 3", Answers2 = "Antwort 4" });
+       d(); }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (radioButton1.Checked)
+            {
+                Console.WriteLine("2");
+            }
+            if (radioButton2.Checked)
+            {
+                Console.WriteLine("sender3");
+            }
+            if (radioButton3.Checked)
+            {
+                Console.WriteLine("sender4");
+            }
+            if (radioButton4.Checked)
+            {
+                Console.WriteLine("sender");
+            }
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            if (index < 4)
+            {
+                index++;
+                d();
+            }
+
+        }
+
+
+        public class QuestionAndAnswers
+        {
+            public string Question { get; set; }
+            public string Answers { get; set; }
+            public string Answers0 { get; set; }
+            public string Answers1 { get; set; }
+            public string Answers2 { get; set; }
+            public int CorrectAnswer { get; set; }
+
+        }
+
+
+            private void d()
+        {
+
+            QuestionAndAnswers question = Questions[index];
+                textBox1.Text = question.Question;
+                radioButton1.Text = question.Answers;
+                radioButton2.Text = question.Answers0;
+                radioButton3.Text = question.Answers1;
+                radioButton4.Text = question.Answers2;
+
+            }
+        }
+    }
+
+
